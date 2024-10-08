80150579828c85c5101a7f810bf8c80e477d8e5f
DianJr <diancarlos29@gmail.com>
2024-10-08 16:35:33 -0300

    tirar h2

--- pizza_land-main/app/components/modal/ModalNewPizza.tsx
+++ pizza_land-main/app/components/modal/ModalNewPizza.tsx
@@ -14,7 +14,6 @@ const ModalNewPizza: React.FC<ModalProps> = ({ isOpen, onClose, addNewPizza }) =
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded shadow-lg overflow-y-auto max-h-[90vh]">
-        <h2 className="text-xl font-bold mb-4">Adicionar Nova Pizza</h2>
         <PizzaForm addNewPizza={addNewPizza} />
         <button onClick={onClose} className="mt-4 bg-red-500 text-white p-2 rounded">Fechar</button>
       </div>
